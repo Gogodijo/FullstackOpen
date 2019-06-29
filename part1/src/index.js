b915cee27e091732db900d870b60eb9d9cbc1823
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2019-06-29 19:14:29 +0300

    asjiodas
    =)

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,36 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const Footer = () => {
-    return (
-        <div>
-            Greetings app created by Joni =)
-        </div>
-    )
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+const App = (props) => {
+  const { notes } = props
+  const rows = () => notes.map(note => <li key={note.id} >{note.content}</li>)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {rows()}
+      </ul>
+    </div>
+  )
 }
 
-const Hello = (props) => {
-    return (
-        <div>
-            <p>Hello {props.name} and you are {props.age} years old</p>
-        </div>
-    )
-}
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
 
-const App = () => {
-    const name1 = "Noora"
-    const age1 = 22
-    return (
-        <>
-            <h1>Greetings!</h1>
-            <Hello name={name1} age={age1} />
-            <Footer/>
-        </>
-    )
-}
+  
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
