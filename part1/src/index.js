a7105ff8a882a33c031a7a0781abb874c7a738e2
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2019-07-01 12:16:22 +0300

    Revert "Update index.js"
    This reverts commit fa7ff83f6079756b06ff1adee20bfe9a891f86c8.

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const notes = [
   {
     id: 1,
-    content: 'HTML is easy. Niin on =)',
+    content: 'HTML is easy',
     date: '2019-05-30T17:30:31.098Z',
     important: true
   },
